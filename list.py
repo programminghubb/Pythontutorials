75a530d697b7d6df7c702daa2cb59e4399677e95
programminghubb <35744285+programminghubb@users.noreply.github.com>
2018-01-30 22:52:29 +0545

    list operations

---
+++ list.py
@@ -0,0 +1,10 @@
+info = ['programminghub', 'steemit' , 2, 'posts', 'python']
+
+info.append('useless')
+print(info)
+info.remove('useless')
+print(info)
+print(info.count('posts'))
+
+info1 = [['programminghub', 'steemit'] ,[ 2, 'posts', 'python']]
+print(info1[0])
