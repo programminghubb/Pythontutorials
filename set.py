eca9d947aa3a45f7e7091d28df839fc945305b3f
programminghubb <35744285+programminghubb@users.noreply.github.com>
2018-01-30 23:56:27 +0545

    set operations

---
+++ set.py
@@ -0,0 +1,8 @@
+x = {'a', 'a', 'b', 'c', 'd', 'e', 'f', 'f', 'f', 'g', 'h'}
+print("Sets after removing repeated elements:")
+print(x)
+y = set("ghijkl")
+print(y)
+print(x-y)
+print(x | y)
+print(x ^ y)
