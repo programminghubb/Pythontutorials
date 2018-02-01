8a67b8a0b9d158b390fba5488369aa0336f70519
programminghubb <35744285+programminghubb@users.noreply.github.com>
2018-02-01 23:28:46 +0545

    function example

---
+++ function.py
@@ -0,0 +1,10 @@
+# Defining function with no parameter
+def hello_world():
+    return "Hello World"
+# Defining function with parameter
+def area_rect(x, y):
+    return x * y
+# printing Hello World
+print(hello_world())
+# printing area of rectangle
+print(area_rect(4, 5))
