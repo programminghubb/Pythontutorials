eaf07ea753342ddecb21e8bfb304739783681c66
programminghubb <35744285+programminghubb@users.noreply.github.com>
2018-02-01 22:08:55 +0545

    dictionary operations

---
+++ dict.py
@@ -0,0 +1,16 @@
+# dictionary declaration
+dict = {'name' : 'SCB', 'branch' : 'London','staffs' : 30 }
+# printing all items from dictionary
+print(dict.items())
+# printing all keys from dictionary
+print(dict.keys())
+# printing all values from dictionary
+print(dict.values())
+# updating value of key 'name'
+dict['name']='SB'
+# printing all items after update
+print(dict.items())
+# adding element to dict
+dict['counters'] = 'three'
+# printing all itemsafter adding element
+print(dict.items())
