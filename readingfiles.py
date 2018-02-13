ecd717f3a529a50234c926865b8fe7ff2b6917b4
programminghubb <35744285+programminghubb@users.noreply.github.com>
2018-02-13 13:56:29 +0545

    Delete readingfiles.py

--- readingfiles.py
+++
@@ -1,6 +0,0 @@
-# Opening a file
-fileobject = open("newfile.txt", "w")
-# writing to file
-fileobject.write("Follow programminghub tutorials to learn python programming.\nLearn to code.\n Learning is great.\n");
-# Closing opened file
-fileobject.close()
