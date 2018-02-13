f6981bdb402308bfd8e1ee92252bc6745a21cb4c
programminghubb <35744285+programminghubb@users.noreply.github.com>
2018-02-13 13:55:24 +0545

    reading files in python

---
+++ readingfiles.py
@@ -0,0 +1,6 @@
+# Opening a file
+fileobject = open("newfile.txt", "w")
+# writing to file
+fileobject.write("Follow programminghub tutorials to learn python programming.\nLearn to code.\n Learning is great.\n");
+# Closing opened file
+fileobject.close()
